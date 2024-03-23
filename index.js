f4e7b33052458a37e492383d90514e758c93864a
Sami3160 <samibhadgaonkar@gmail.com>
2024-03-23 12:34:02 +0530

    Update CSS styles and fix broken links

--- index.js
+++ index.js
@@ -36,22 +36,49 @@ function onSemClick(context) {
 
   //creating divs
   const semisterDiv= document.createElement('div');
-  semisterDiv.className="semisterDiv w-full grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 m-3 p-2 border-2 border-gray-300 rounded-md overflow-y-scroll space-x-1 ";
+  semisterDiv.className="semisterDiv w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 lg:m-3 p-2 border-2 border-gray-300 rounded-md overflow-y-scroll space-x-1 ";
   semisterDiv.id="semisterDiv";
   syllabusContainer.appendChild(semisterDiv)
 
   //creating cards
-  subjectArray.map((obj)=>{
+  subjectArray.map((obj,index)=>{
     const cards=document.createElement("div")
-    cards.className=" p-2  my-4 shadow-lg hover:bg-purple-200 flex flex-col  font-mono cursor-pointer"
+    cards.className="cseCard bg-purple-100 p-2  my-4 shadow-lg hover:bg-purple-200 flex flex-col min-w-48 font-mono cursor-pointer"
     cards.setAttribute("onclick","handleCardClick(this)")
+    // cards.setAttribute("onclick","handleCardClick(this)")
+    cards.setAttribute("subject-info", subjectRef[context.innerText]+":"+index)
     const info=document.createElement('div')
     info.className="info text-thin text-xl"
     info.innerHTML=obj["name"]
+    
     // info.innerHTML(obj["name"])
     const description=document.createElement('div')
+    const dataUl=document.createElement('ul')
+    const theoryLi=document.createElement('li')
+    const practicalLi=document.createElement('li')
+    const externalLi=document.createElement('li')
+    const projectLi=document.createElement('li')
+
+    dataUl.className="flex flex-col p-1 list-none text-xs md:text-sm"
+    theoryLi.className="text-sm"
+    practicalLi.className="text-sm"
+    externalLi.className="text-sm"
+    projectLi.className="text-sm"
+
+    theoryLi.innerText="Theory Exam :"+ obj["props"].isTheoryTest
+    practicalLi.innerText="Practical Exam :"+ obj["props"].isPracticalTest
+    externalLi.innerText="Project :"+ obj["props"].isProject
+    projectLi.innerText="External :"+ obj["props"].isExternal
+
+    dataUl.appendChild(theoryLi);
+    dataUl.appendChild(practicalLi);
+    dataUl.appendChild(externalLi);
+    dataUl.appendChild(projectLi);
+
+
+
     description.className="description mt-4 text-sm p-2 bg-gray-200 shadow-inner rounded-xl"
-    description.innerHTML="schs"
+    description.appendChild(dataUl)
     cards.appendChild(info)
     cards.appendChild(description)
     semisterDiv.appendChild(cards)
@@ -65,106 +92,135 @@ function onSemClick(context) {
 
 }
 
-function handleCardClick(){
+function handleCardClick(context){
+  const syllabusContainer=document.getElementById("syllabus-container")
+  while(syllabusContainer.firstChild){
+    syllabusContainer.removeChild(syllabusContainer.firstChild)
+  }
+
+  const [semNo, subjectIndex]=context.getAttribute("subject-info").split(":")
+  const semData = data[semNo]
+  const subjectInfo = semData.subject[0][subjectIndex]
+  console.log(semNo)
+  console.log(subjectInfo)
+
+  const semisterDiv= document.createElement('div');
+  semisterDiv.className="semisterDiv w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 lg:m-3 p-2 border-2 border-gray-300 rounded-md overflow-y-scroll space-x-1 ";
+  semisterDiv.id="semisterDiv";
+  syllabusContainer.appendChild(semisterDiv)
+
+  const notesData= subjectInfo.props.notes;
+  const syllabusData = subjectInfo.props.syllabus;
+  const questionPaperData = subjectInfo.props.questionPapers;
+  const referenceBookData = subjectInfo.props.referenceBooks;
+
+
+  // syllabus cards code
+  const syllabusComponent = document.createElement("div")
+  syllabusComponent.className="col-span-1 min-w-24 flex flex-col space-y-4 bg-purple-100 shadow-xl p-2  text-xs sm:text-sm hover:bg-purple-200 flex flex-col font-thin my-4"
+  const info=document.createElement('div')
+  info.innerHTML=`[Java ${subjectInfo.name}]`
+  const a=document.createElement("a");
+  a.href=syllabusData
+  a.target="_blank"
+  a.title="Download or View"
+  a.innerHTML="Download"
+  a.className="inline-block text-white bg-purple-700 rounded-full p-2 text-sm cursor-pointer hover:bg-purple-900 text-center"
+  syllabusComponent.appendChild(info)
+  syllabusComponent.appendChild(a)
+  semisterDiv.appendChild(syllabusComponent)
+
+
+
+
+  // notes cards code
+  notesData.map((obj, index)=>{
+    const notesComponent= document.createElement("div")  
+    const info=document.createElement('div')
+    const a=document.createElement("a");
+    const no=document.createElement("p")
+
+    notesComponent.className="col-span-1 min-w-24 flex flex-col  space-y-4 bg-purple-100 shadow-xl p-2  text-xs sm:text-sm hover:bg-purple-200 flex flex-col font-thin my-4"
+    info.innerHTML=`[Notes for ${subjectInfo.name}]`
+    no.className="text-2xl font-extrabold p-2"
+    no.innerHTML=`#${index+1}`
+    a.href=obj.note
+    a.target="_blank"
+    a.title="Download or View"
+    a.innerHTML="Download"
+    a.className="inline-block text-white bg-purple-700 rounded-full p-2 text-sm cursor-pointer hover:bg-purple-900 text-center"
+    
+    notesComponent.appendChild(no)
+    notesComponent.appendChild(info)
+    notesComponent.appendChild(a)
+    semisterDiv.appendChild(notesComponent )
+  })
+
+
+
+  // question bank cards code
+  questionPaperData.map((obj, index)=>{
+    const questionPaperComponent= document.createElement("div")  
+    const info=document.createElement('div')
+    const info2=document.createElement("div")
+    const a=document.createElement("a");
+    const no=document.createElement("p")
+
+
+    questionPaperComponent.className="col-span-1 min-w-24 flex flex-col space-y-4 bg-purple-100 shadow-xl p-2  text-xs sm:text-sm hover:bg-purple-200 flex flex-col font-thin my-4"
+    info.innerHTML=`[Question papers: ${subjectInfo.name}]`
+    info2.innerHTML=`Year: ${obj.year}`
+    no.className="text-2xl font-extrabold p-2"
+    no.innerHTML=`#${index+1}`
+    a.href=obj.questionPaper
+    a.target="_blank"
+    a.title="Download or View"
+    a.innerHTML="Download"
+    a.className="inline-block text-white bg-purple-700 rounded-full p-2 text-sm cursor-pointer hover:bg-purple-900 text-center"
+    
+
+    questionPaperComponent.appendChild(no)
+    questionPaperComponent.appendChild(info)
+    questionPaperComponent.appendChild(info2)
+    questionPaperComponent.appendChild(a)
+    semisterDiv.appendChild(questionPaperComponent )
+  })
+
+
+  // reference book cards code
+  referenceBookData.map((obj, index)=>{
+    const referenceBookComponent= document.createElement("div")  
+    const info=document.createElement('div')
+    const info2=document.createElement('div')
+    const info3=document.createElement('div')
+    const a=document.createElement("a");
+    const no=document.createElement("p")
+
+
+    referenceBookComponent.className="col-span-1 min-w-24 flex flex-col space-y-4 bg-purple-100 shadow-xl p-2  text-xs sm:text-sm hover:bg-purple-200 flex flex-col font-thin my-4"
+    info.innerHTML=`[Reference books for ${subjectInfo.name}]`
+    info2.innerHTML=`Year: ${obj.year}`
+    info3.innerHTML=`Author: ${obj.author}`
+    no.className="text-2xl font-extrabold p-2"
+    no.innerHTML=`#${index+1}`
+    a.href=obj.book
+    a.target="_blank"
+    a.title="Download or View"
+    a.innerHTML="Download"
+    a.className="inline-block text-white bg-purple-700 rounded-full p-2 text-sm cursor-pointer hover:bg-purple-900 text-center"
+    
+
+    referenceBookComponent.appendChild(no)
+    referenceBookComponent.appendChild(info)
+    referenceBookComponent.appendChild(info2)
+    referenceBookComponent.appendChild(info3)
+    referenceBookComponent.appendChild(a)
+    semisterDiv.appendChild(referenceBookComponent)
+  })
+  
+
+
+  
+  info.className="info text-thin text-xl"
 
 }
-
-function temp() {
-  alert("hello")
-}
-
-
-// const CSENotesPerSEM = {
-//   sem3: {
-//     sem: 3,
-//     syllabus: ["css", "js"],
-//     syllabusImg: ["/img/css.png", "/img/js.png"],
-//     syllabusLink: [
-//       "/notes/CSS_Complete_Notes (2) (1).pdf",
-//       "/notes/JavaScript Chapter 1 - Practice Set.pdf",
-//     ],
-//   },
-//   sem4: {
-//     sem: 4,
-//     syllabus: ["java", "python", "html"],
-//     syllabusImg: ["/img/java.svg","/img/python.svg","/img/html.png"],
-//     syllabusLink: ["/chapter_wise_notes/Java_ChapterWise_Notes.zip","/chapter_wise_notes/Python_ChapterWise_Notes.zip","/chapter_wise_notes/HTML_ChapterWise_Notes.zip"],
-//   },
-//   sem5: {
-//     sem: 5,
-//     syllabus: ["c", "ts", "php"],
-//     syllabusImg: ["/img/c.png","/img/ts.png","/img/php.svg"],
-//     syllabusLink: ["/chapter_wise_notes/C_ChapterWise_Notes.zip","/notes/JS_Chapterwise_Notes (1).pdf"],
-//   },
-//   sem6: {
-//     sem: 6,
-//     syllabus: ["linux", "dsa"],
-//     syllabusImg: ["/img/linux.png","/img/dsa.png"],
-//     syllabusLink: ["/notes/Linux Helpful Commands List.pdf","/notes/DSA_CompleteNotes (1).pdf"],
-//   },
-// };
-// var buttonIds = ["sem3", "sem4", "sem5", "sem6"];onSemClick
-
-// // // Loop through the array of button IDs
-// // buttonIds.forEach(function (id) {
-// //   // Get the button element by its ID
-// //   var button = document.getElementById(id);
-
-// //   // Add onclick event listener
-// //   button.addEventListener("click", function () {
-// //     getSyllabus(id);
-// //   });
-// // });
-
-// const getSyllabus = (sem) => {
-//   console.log(CSENotesPerSEM[sem].syllabus);
-
-//   const syllabusContainer = document.getElementById("syllabus-container");
-//   while (syllabusContainer.firstChild) {
-//     syllabusContainer.removeChild(syllabusContainer.firstChild);
-//   }
-//   //const semesterDiv = document.createElement("div");
-//   for (const subject of CSENotesPerSEM[sem].syllabus) {
-//     // Create a new div for each subject
-//     const subjectDiv = document.createElement("div");
-//     subjectDiv.className = "subject";
-//     subjectDiv.innerText = subject;
-
-//     // Create a new div for the image
-//     const imageDiv = document.createElement("div");
-//     imageDiv.className = "syllabusimage";
-
-//     // Get the image source from CSENotesPerSEM
-//     const imageSrc =
-//       CSENotesPerSEM[sem].syllabusImg[
-//         CSENotesPerSEM[sem].syllabus.indexOf(subject)
-//       ];
-
-//     // Create an image element and set its source
-//     const image = document.createElement("img");
-//     image.src = imageSrc;
-
-//     // Append the image to the image div
-//     imageDiv.appendChild(image);
-
-//     // Create an a tag with a button inside it
-//     const aTag = document.createElement("a");
-//     aTag.href =
-//       CSENotesPerSEM[sem].syllabusLink[
-//         CSENotesPerSEM[sem].syllabus.indexOf(subject)
-//       ];
-//     const button = document.createElement("button");
-//     button.className = "subjectbutton";
-//     button.innerText = "Go to syllabus";
-
-//     aTag.appendChild(button);
-
-//     // Append the image div to the subject div
-//     subjectDiv.appendChild(imageDiv);
-//     subjectDiv.appendChild(aTag);
-
-//     //semesterDiv.appendChild(subjectDiv);
-//     syllabusContainer.appendChild(subjectDiv);
-//   }
-// };
-
